3d35f1c194f2a79a6e0f024bbd17fff7e405e7a9
Guðni Már Gilbert <gudnimar@noxmedical.com>
2022-07-14 14:36:11 +0000

    Reading CO2 values now works

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from machine import SoftI2C
+import SCD41
 
 # create software I2C object
-i2c = SoftI2C(scl='X9', sda='X10', freq=400000)
+i2c : SoftI2C = SoftI2C(scl='X9', sda='X10', freq=400000)
 
 # returns list of peripheral addresses
 i2cAddresses : list = i2c.scan()
 
 # SCD41 sensor should have I2C address 0x62
 print("List of I2C addresses: %s" % [hex(addr) for addr in i2cAddresses])
+
+SCD41.read_measurement(i2c)
