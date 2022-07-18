93fb226d2c6b71678107dbf143cf4d3ddb7acc90
Guðni Már Gilbert <gudnimar@noxmedical.com>
2022-07-18 11:02:32 +0000

    Implement altitude and pressure commands

--- SCD41.py
+++ SCD41.py
@@ -35,6 +35,8 @@ MEASURE_SINGLE_SHOT_RHT_ONLY = 0x2196
 POWER_DOWN = 0x36E0
 WAKE_UP = 0x36F6
 
+isIdle: bool = True
+
 
 def crc8(data: bytearray) -> int:
     """
@@ -56,31 +58,9 @@ def start_periodic_measurement(i2c: SoftI2C) -> None:
     """
     Start periodic measurement, signal update interval is 5 seconds.
     """
+    global isIdle
     i2c.writeto_mem(0x62, START_PERIODIC_MEASUREMENT, b"", addrsize=16)
-
-
-def stop_periodic_measurement(i2c: SoftI2C) -> None:
-    """
-    Stop periodic measurement to change the sensor configuration or to save power.
-    Note that the sensor will only respond to other commands after waiting 500 ms
-    after issuing the stop_periodic_measurement command.
-    """
-    i2c.writeto_mem(0x62, STOP_PERIODIC_MEASUREMENT, b"", addrsize=16)
-    time.sleep(0.5)  # Sensor becomes responsive after 500ms
-
-
-def is_data_ready(i2c: SoftI2C) -> bool:
-    buf: bytearray = bytearray(3)
-    i2c.readfrom_mem_into(0x62, GET_DATA_READY_STATUS, buf, addrsize=16)
-    return ((buf[0] & 0x7) << 8) | buf[1] != 0
-
-
-# TODO
-# def set_temperature_offset(i2c: SoftI2C) -> None:
-# def get_temperature_offset(i2c: SoftI2C) -> None:
-# def set_sensor_altitude(i2c: SoftI2C) -> None:
-# def get_sensor_altitude(i2c: SoftI2C) -> None:
-# def set_ambient_pressure(i2c: SoftI2C, pressure_pa : int) -> None:
+    isIdle = False
 
 
 def read_measurement(i2c: SoftI2C) -> None:
@@ -119,3 +99,96 @@ def read_measurement(i2c: SoftI2C) -> None:
             % (hex(buf[8]), hex(crc8(buf[6:8])))
         )
     print("Humidity: %.2f%%" % relative_humidity)
+
+
+def stop_periodic_measurement(i2c: SoftI2C) -> None:
+    """
+    Stop periodic measurement to change the sensor configuration or to save power.
+    Note that the sensor will only respond to other commands after waiting 500 ms
+    after issuing the stop_periodic_measurement command.
+    """
+    global isIdle
+    i2c.writeto_mem(0x62, STOP_PERIODIC_MEASUREMENT, b"", addrsize=16)
+    isIdle = True
+    time.sleep(0.5)  # Sensor becomes responsive after 500ms
+
+
+def is_data_ready(i2c: SoftI2C) -> bool:
+    buf: bytearray = bytearray(3)
+    i2c.readfrom_mem_into(0x62, GET_DATA_READY_STATUS, buf, addrsize=16)
+    return ((buf[0] & 0x7) << 8) | buf[1] != 0
+
+
+# TODO
+# def set_temperature_offset(i2c: SoftI2C) -> None:
+
+# TODO
+# def get_temperature_offset(i2c: SoftI2C) -> None:
+
+
+def set_sensor_altitude(i2c: SoftI2C, altitude: int) -> None:
+    """
+    Get the set sensor altitude. The altitude represenets meters above sea level.
+    NOTE: Sensor must be in IDLE mode
+    """
+    if isIdle == False:
+        print("# Error: Sensor is not IDLE")
+        return
+
+    buf: bytearray = bytearray(3)
+    altitude_bytes = altitude.to_bytes(2, "big")
+    buf[0] = altitude_bytes[0]  # MSB
+    buf[1] = altitude_bytes[1]  # LSB
+    buf[2] = crc8(buf[0:2])  # CRC
+    i2c.writeto_mem(0x62, SET_SENSOR_ALTITUDE, buf, addrsize=16)
+
+    # Store altitude in EEPROM
+    persist_settings(i2c)
+
+
+def get_sensor_altitude(i2c: SoftI2C) -> None:
+    """
+    Get the set sensor altitude. The altitude represenets meters above sea level.
+    NOTE: Sensor must be in IDLE mode
+    """
+    if isIdle == False:
+        print("# Error: Sensor is not IDLE")
+        return
+
+    buf: bytearray = bytearray(3)
+    i2c.readfrom_mem_into(0x62, GET_SENSOR_ALTITUDE, buf, addrsize=16)
+    if crc8(buf[0:2]) != buf[2]:
+        print(
+            "Received CRC %s did not match calculated CRC %s"
+            % (hex(buf[2]), hex(crc8(buf[0:2])))
+        )
+
+    altitude = buf[0] << 8 | buf[1]
+    print("Altitude is %s m" % altitude)
+
+
+def set_ambient_pressure(i2c: SoftI2C, pressure: int) -> None:
+    """
+    The set_ambient_pressure command can be sent during periodic measurements to enable continuous pressure
+    compensation. Note that setting an ambient pressure using set_ambient_pressure overrides any pressure compensation based
+    on a previously set sensor altitude. Use of this command is highly recommended for applications experiencing significant ambient
+    pressure changes to ensure sensor accuracy
+    """
+    if isIdle == False:
+        print("# Error: Sensor is not IDLE")
+        return
+
+    buf: bytearray = bytearray(3)
+    pressure = int(pressure / 100) # Signal conversion
+    pressure_bytes = pressure.to_bytes(2, "big")
+    buf[0] = pressure_bytes[0]  # MSB
+    buf[1] = pressure_bytes[1]  # LSB
+    buf[2] = crc8(buf[0:2])  # CRC
+    i2c.writeto_mem(0x62, SET_AMBIENT_PRESSURE, b"", buf, addrsize=16)
+
+    # Store pressure in EEPROM
+    persist_settings(i2c)
+
+
+def persist_settings(i2c: SoftI2C) -> None:
+    i2c.writeto_mem(0x62, PERSIST_SETTINGS, b"", addrsize=16)
--- main.py
+++ main.py
@@ -10,5 +10,14 @@ i2cAddresses : list = i2c.scan()
 # SCD41 sensor should have I2C address 0x62
 print("List of I2C addresses: %s" % [hex(addr) for addr in i2cAddresses])
 
+# Ensure Sensor starts in IDLE mode at boot-up
+SCD41.stop_periodic_measurement(i2c)
+
+# Set current altitude
+print("Setting Altitude to 41m")
+SCD41.set_sensor_altitude(i2c, 41)
+
 while(True):
+    SCD41.stop_periodic_measurement(i2c)
+    SCD41.get_sensor_altitude(i2c)
     SCD41.read_measurement(i2c)
